f2197a5c0fcc618572a369a401cf468f1b005582
aikasi <32337856+aikasi@users.noreply.github.com>
2021-04-05 05:25:25 +0900

    feat: 12354

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -40,5 +40,5 @@ export class MoviesController {
   path(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
     return this.moviesService.update(movieId, updateData);
   }
-  //1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+  //1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17
 }
